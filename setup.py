feb2f2dfa56d6152ad7274112c764b68aa463765
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-08-04 13:39:22 +0800

    fix: fix LICENSE

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL-2.0)",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: OS Independent",
     ],
     license="MPL-2.0",
